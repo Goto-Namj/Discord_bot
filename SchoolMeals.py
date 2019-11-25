2fa163ccfd12b8789a8254fb18cf6274299127bd
goto <45956673+Goto-Namj@users.noreply.github.com>
2019-11-26 08:56:27 +0900

    급식봇 개선
    어제 한 일....

--- SchoolMeals.py
+++ SchoolMeals.py
@@ -1,65 +1,108 @@
 import discord
 import asyncio
-from urllib.request import urlopen
+import setting
+import datetime
+import requests
 from bs4 import BeautifulSoup
-
-client = discord.Client()
-
-token = "NjM4MTY4NTY1Nzk0NjAzMDE5.XbZuOQ.dSdKmI3Mzu9tlyTiZsE7fSTwmyI"
-
-ch = 0
-def sl(a):
-    global ch
-    for i in range(128):
-        if ord(a) == 38:
-            if ch == 0:
-                ch = 1
-                return ''
-            return a
-        if ord(a) == i:
-            return ''
-    return a
-
-@client.event
-async def on_ready():
-    print("이거이거 : ", client.user.name, client.user.id)
-    game ="크롤링"
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(game))
-
-@client.event
-async def on_message(message):
-    if '급식' in message.content:
-        html = urlopen("http://www.gsm.hs.kr/main/main.php")  
-        bsObject = BeautifulSoup(html, "html.parser")
-        l,iff,c,li = map(sl,str(bsObject.dd)),0,0,['']
-        global ch
-        ch = 0
-        tmp = 0
-        for i in l:
-            if i != '':
-                tmp = 0
-                if i == '&':
-                    tmp = 1
-                iff = 1
-                li[c] += i
-            elif iff == 1 and tmp == 0:
-                li.append('')
-                iff=0
-                c+=1
-                tmp = 0
-    
-        result = ''
-        for i in li:
-            if i == '에너지':
-                break
-            result += i+'\n'
-
-        channel = message.channel
-        await channel.send("```"+result+"```")
+import re
 
 
-    # if message.content == "!ping":
-    #     channel = message.channel
-    #     await channel.send('pong')
+token = setting.token
 
-client.run(token)
+def nowtime():
+    dt = str(datetime.datetime.now())
+    time = {
+        'year': dt[:4],
+        'month': dt[5:7],
+        'day': dt[8:10],
+        'time': int(dt[11:13]) * 60 + int(dt[14:16]),
+        'ymd': dt[:10]
+    }
+    return time
+
+def nexttime():
+    next_time = datetime.datetime.now() + datetime.timedelta(days = 1) 
+    dt = str(next_time)
+    time = {
+        'year': dt[:4],
+        'month': dt[5:7],
+        'day': dt[8:10],
+        'time': int(dt[11:13]) * 60 + int(dt[14:16]),
+        'ymd': dt[:10]
+    }
+    return time
+
+def crawling_split(time, re_pattern, amp_pattern):
+    # def crawling(time, re_pattern):
+    url = f"https://stu.gen.go.kr/sts_sci_md00_001.do?schulCode=F100000120&schulCrseScCode=4&schulKndScCode=04&ay={time['year']}&mm={time['month']}"
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, "html.parser")
+    items = soup.select("#contents > div > table > tbody > tr > td > div")
+    #return items
+
+    # def split(time, items):
+    menu = {}
+    for i in items:
+        si = str(i)
+        if si.startswith('<div>' + time['day'] + '<'):
+
+            si = amp_pattern.findall(si)
+            ri = ''
+            for j in si:
+                ri+=j
+            si = ri.split('[')
+            del si[0]
+            menu['breakfast'] = re_pattern.findall(si[0][2:])
+            menu['lunch'] = re_pattern.findall(si[1][2:])
+            menu['dinner'] = re_pattern.findall(si[2][2:])
+            break
+    return menu
+
+
+class SMBot(discord.Client):
+    async def on_ready(self):
+        self.amp_pattern = re.compile("[^amp;]+")
+        self.re_pattern = re.compile("[가-힣\s]*&?[가-힣\s]+")
+        self.time = nowtime()
+        self.menu = crawling_split(self.time, self.re_pattern, self.amp_pattern)
+        self.endtime = {
+            'breakfast': (  8  )*60+  0,
+            'lunch': (12+  1  )*60+  0,
+            'dinner': (12+  7  )*60+  0
+        }
+
+        game ="일"
+        print(self.user.name, self.user.id)
+        await self.change_presence(status=discord.Status.online, activity=discord.Game(game))
+
+    async def on_message(self, message):
+        if '급식' in message.content and not message.author.bot:
+            channel = message.channel
+            report = ''
+            time = nowtime()
+
+            if self.time['ymd'] != time['ymd']:
+                if self.time['time'] >= self.endtime['dinner']:
+                    time = nexttime()
+                    self.menu = crawling_split(time, self.re_pattern, self.amp_pattern)
+                else:
+                    self.time = time
+                    self.menu = crawling_split(self.time, self.re_pattern, self.amp_pattern)
+
+            if self.time['time'] < self.endtime['breakfast']:
+                menu = self.menu['breakfast']
+            elif self.time['time'] < self.endtime['lunch']:
+                menu = self.menu['lunch']
+            elif self.time['time'] < self.endtime['dinner']:
+                menu = self.menu['dinner']
+            else:#self.time['time'] >= self.endtime['dinner']:
+                menu = self.menu['breakfast']
+                
+            for i in menu:
+                report += i+'\n'
+
+            print(message.author, datetime.datetime.now())
+            await channel.send("```"+report+"```")
+
+bot = SMBot()
+bot.run(token)
